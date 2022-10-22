c916b62ea5ddc90d20fcb5a6a36b91f93a976f57
Mona Lisa <holmesmainsend@gmail.com>
2022-10-22 16:28:59 -0400

    finished adding user prompts

--- madlibs.c
+++ madlibs.c
@@ -4,9 +4,16 @@
 int main() {
     char color[20], pluralNoun[20], celebrity[20];
 
-    printf("Roses are {color}\n");
-    printf("{plural-noun} are blue\n");
-    printf("I love {celebrity}\n");
+    printf("Enter a color of no more than 20 characters: ");
+    scanf("%s", color);
+    printf("Enter a plural noun of no more than 20 characters: ");
+    scanf("%s", pluralNoun);
+    printf("Enter a celebrity of no more than 20 characters: ");
+    scanf("%s", celebrity);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s\n", celebrity);
 
     return 0;
 }
