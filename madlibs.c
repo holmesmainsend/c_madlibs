27ff0f19ce552114c5670dc30f047ddeba8eb1de
Mona Lisa <holmesmainsend@gmail.com>
2022-10-22 16:25:49 -0400

    added initial string variables

--- madlibs.c
+++ madlibs.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    
+    char color[20], pluralNoun[20], celebrity[20];
+
+    printf("Roses are {color}\n");
+    printf("{plural-noun} are blue\n");
+    printf("I love {celebrity}\n");
 
     return 0;
 }
